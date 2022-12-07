63c66bf38c7d9764ba39d1b5bbda1f29d1879986
Ana Bindiu <anabindiu11@gmail.com>
2022-12-07 01:25:16 -0700

    added chat box --> but it needs fixing also got this for reference from somewhere else just to understand it first then i will change it accordingly so far server and client aren't connecting

--- routes/Messaging_Page.js
+++ routes/Messaging_Page.js
@@ -1,8 +1,28 @@
+//used this for reference: https://sabe.io/tutorials/how-to-build-real-time-chat-app-node-express-socket-io
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Welcome to the messaging page');
+router.get('/', function(req, res) {
+  res.sendFile(__dirname + "/ChatBox.html");
 });
+
+module.exports = function(io){
+    io.on('connection', (socket) => {
+        socket.on("user_join", function(data) {
+            this.username = data;
+            socket.broadcast.emit("user_join", data);
+        });
+    
+        socket.on("chat_message", function(data) {
+            data.username = this.username;
+            socket.broadcast.emit("chat_message", data);
+        });
+    
+        socket.on("disconnect", function(data) {
+            socket.broadcast.emit("user_leave", this.username);
+        });
+    })
+
+};
 
 module.exports = router;
