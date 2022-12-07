98a7f58e9c663ccc7a8bf381aaafcefa079181f5
Ana Bindiu <anabindiu11@gmail.com>
2022-12-07 00:20:58 -0700

    added changes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+var messagingRouter = require('./routes/Messaging_Page');
+
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 const mongoString = process.env.DATABASE_URL;
@@ -33,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Messaging_Page', messagingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
