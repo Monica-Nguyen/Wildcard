f452883bb5f9839c9977427c0363fa94769e7ef1
Monica Nguyen <monicanguyen@monicangu.hitronhub.home>
2022-12-11 13:19:31 -0700

    user can connect now

--- routes/chat.js
+++ routes/chat.js
@@ -1,25 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-var io = require("socket.io")(http);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-});
-
-server.listen(8080, () => {
-  console.log(8080);
-});
+// const app = express();
+// const http = require("http");
+// const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// var io = require("socket.io")(server);
 
 router.get("/", function (req, res, next) {
   res.render("chat", {});
 });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+// });
+
+// server.listen(8080, () => {
+//   console.log(8080);
+// });
 
 module.exports = router;
