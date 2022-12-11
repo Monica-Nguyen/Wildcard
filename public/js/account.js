0290d953f3d8dee7539cf2c79cca485d420e23dc
smitra33 <60757186+smitra33@users.noreply.github.com>
2022-12-11 13:01:53 -0700

    Started edit job capabilities in employer form and now will open specified job in the job space

--- public/js/account.js
+++ public/js/account.js
@@ -1,6 +1,22 @@
 let totalJobs;
 let company;
 let companyID;
+const opts = {
+    plugins: ['remove_button'],
+    create: true,
+    onItemAdd:function(){
+        this.setTextboxValue('');
+        this.refreshOptions();
+    },
+    render:{
+        option:function(data,escape){
+            return '<div class="d-flex"><span>' + escape(data.value) + '</span><span class="ms-auto text-muted"></span></div>';
+        },
+        item:function(data,escape){
+            return '<div>' + escape(data.value) + '</div>';
+        }
+    }
+}
 
 async function checkJobDetails() {
     let companyData;
@@ -18,26 +34,50 @@ async function checkJobDetails() {
             company = companyData[0]["company_name"];
             companyID = companyData[0]["_id"];
             let jobs = companyData[0]["jobs"];
-            if (jobs) {
-                totalJobs = jobs;
-                //TODO MAKE JOB SECTION AND PREFILL NAME
-            }
+            totalJobs = jobs;
+            await getJobTitles();
             addJobCreateOptions();
         }
     })
+}
+
+async function getJobTitles() {
+    let title;
+    let level;
+    if (totalJobs.length > 0) {
+        let jobsInfo = `<div class="mb-3"><h5 class="form-headers">Current Jobs</h5></div>`;
+        for (let i = 0; i<totalJobs.length; i++) {
+            let job = await getJobTitle(totalJobs[i]);
+            title = job[0]['position_title'];
+            level = job[0]['position_level'];
+            jobsInfo += `<span class="input-group-btn"><div>${title} : ${level}<button class="btn" onclick="editJob(job[0]['_id'])"><span class="material-symbols-outlined">edit</span></button></div></span>`;
+        }
+        document.getElementById('job-list').innerHTML = jobsInfo;
+    }
+}
+
+async function getJobTitle(job) {
+    let jobURL = 'http://localhost:3000/api/job/' + job
+    let response = await fetch(jobURL, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+        },
+    });
+    return await response.json();
 }
 
 function addJobCreateOptions() {
     let companyOption = document.getElementById('company-option');
     let newOptions =
         `
+        <div class="card-space"></div>
         <div class="mb-3">
-            <p>Add Jobs to your Employee Profile</p>
+            <h5 class="form-headers">Add Jobs to your Employee Profile</h5>
         </div>
         <div id="current-jobs"></div>
         <div class="mb-3">
-            <button class="btn" onclick="addNewJobs()"><span class="material-symbols-outlined">add_circle</span></button> Add Job
-        </div>
+            <button class="btn" onclick="addNewJobs()"><span class="material-symbols-outlined">add_circle</span></button> Add Job </div>
         `
     companyOption.innerHTML += newOptions;
     document.getElementById("company-name-input").placeholder = company;
@@ -74,44 +114,120 @@ async function createCompany() {
 }
 
 async function addNewJobs() {
-    let allSkills = await getSkillDetailSelect();
-    let skillSelect = document.getElementById("select-tags-skills");
-    for (let i = 0; i < allSkills.skills.length; i++) {
-        let opt = document.createElement('option');
-        opt.value = allSkills.skills[i];
-        opt.innerHTML = allSkills.skills[i];
-        skillSelect.appendChild(opt);
-    };
+    document.getElementById("job-space-fill").innerHTML =
+        `
+        <div class="card-space"></div>
+        <div class="flex-container">
+            <div id="position_title" class="job-form-fields">
+                <label class="form-label">Position Title</label>
+                <input style="width: 600px" type="position-title" class="form-control" id="position-title-input">
+                <div class="error-validation" id="position-error"></div>
+            </div>
+            <div id="position_level" class="job-form-fields">
+                <label class="form-label">Position Level</label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="position_level" value="Intern" id="intern" checked/>
+                    <label class="form-check-label" for="intern"> Intern </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="position_level" value="Junior" id="junior"/>
+                    <label class="form-check-label" for="junior"> Junior </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="position_level" value="Intermediate" id="intermediate"/>
+                    <label class="form-check-label" for="intermediate"> Intermediate </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="position_level" value="Senior" id="senior"/>
+                    <label class="form-check-label" for="senior"> Senior </label>
+                </div>
+            </div>
+            <div id="can_coach" class="job-form-fields">
+                <label class="form-label">Can coach candidate</label>
+                <input class="form-check-input" type="radio" name="can_coach" value="Yes" id="Yes" checked/>
+                <label class="form-check-label" for="Yes"> Yes </label>
+                <input class="form-check-input" type="radio" name="can_coach" value="No" id="No"/>
+                <label class="form-check-label" for="No"> No </label>
+            </div>
+            <div class="container">
+                <div class="card-space"></div>
+                <label class="form-label">Add Skills</label>
+                <div class="row">
+                    <div class="p-4">
+                        <select style="width: 600px" id="select-tags-skills" multiple>
+                            <option value="JavaScript">JavaScript</option>
+                            <option value="Java">Java</option>
+                            <option value="Python">Python</option>
+                            <option value="HTML">HTML</option>
+                            <option value="CSS">CSS</option>
+                            <option value="React">React</option>
+                            <option value="Angular">Angular</option>
+                            <option value="Node">Node</option>
+                            <option value="AWS">AWS</option>
+                            <option value="Azure">AWS</option>
+                            <option value="Docker">Docker</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="card-space"></div>
+                <label class="form-label">Add Job Details</label>
+                <div class="row">
+                    <div class="p-4">
+                        <select style="width: 600px" id="select-tags-details" multiple>
+                            <option value="Agile">Agile</option>
+                            <option value="Scrum">Scrum</option>
+                            <option value="Remote">Remote</option>
+                            <option value="Hybrid">Hybrid</option>
+                            <option value="Flexible">Flexible</option>
+                            <option value="Waterfall">Waterfall</option>
+                            <option value="Six Sigma">Six Sigma</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="card-space"></div>
+            <button id="create-save-job" class="btn btn-primary" onclick="createJob()" type="button"> Save </button>
+        </div>
+        `
+    new TomSelect("#select-tags-skills", opts);
+    new TomSelect("#select-tags-details", opts);
 }
 
 async function createJob() {
     let positionTitle = $('#position-title-input').val();
-    let positionLevel = document.querySelector('input[name="position_level"]:checked').value;
-    let canCoach = document.querySelector('input[name="can_coach"]:checked').value;
-    let skillItems = $('#select-tags-skills').val();
-    let detailItems = $('#select-tags-details').val();
-    if (canCoach == 'Yes'){
-        canCoach = true;
+    if (positionTitle == "") {
+        document.getElementById("position-error").innerHTML = "Position Title cannot be blank";
     }
-    else {
-        canCoach = false;
+    else{
+        let positionLevel = document.querySelector('input[name="position_level"]:checked').value;
+        let canCoach = document.querySelector('input[name="can_coach"]:checked').value;
+        let skillItems = $('#select-tags-skills').val();
+        let detailItems = $('#select-tags-details').val();
+        if (canCoach == 'Yes'){
+            canCoach = true;
+        }
+        else {
+            canCoach = false;
+        }
+        let jobURL = 'http://localhost:3000/api/job/company'
+        let response = await fetch(jobURL, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "company_name": company,
+                "position_title": positionTitle,
+                "position_level": positionLevel,
+                "can_coach": canCoach,
+                "is_active": true,
+                "skills": skillItems,
+                "preferred_job_details": detailItems
+            })
+        });
+        let data = await response.json();
     }
-    let jobURL = 'http://localhost:3000/api/job/company'
-    let response = await fetch(jobURL, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "company_name": company,
-            "position_title": positionTitle,
-            "position_level": positionLevel,
-            "can_coach": canCoach,
-            "is_active": true,
-            "skills": skillItems,
-            "preferred_job_details": detailItems
-        })
-    });
-    let data = await response.json();
-    console.log(data);
 }
+
