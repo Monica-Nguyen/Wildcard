0decc0fac6b75e201aa05665cde81adf73527efa
Monica Nguyen <monicanguyen@monicangu.hitronhub.home>
2022-12-11 17:05:07 -0700

    inserted the chat html into the chat page

--- routes/chat.js
+++ routes/chat.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const app = express();
-// const http = require("http");
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// var io = require("socket.io")(server);
 
 router.get("/", function (req, res, next) {
   res.render("chat", {});
 });
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-// });
-
-// server.listen(8080, () => {
-//   console.log(8080);
-// });
 
 module.exports = router;
