e29e75e0f893294587ad99dd078a9ff338c9c73f
Avinash Chandiramani <Avinash.Chandiramani@sait.ca>
2022-12-11 17:34:42 -0700

    model changes

--- routes/api.js
+++ routes/api.js
@@ -278,12 +278,68 @@ router.get('/user/all', async (req, res) => {
     }
 })
 
-router.get('/discover/yes', function(req, res, next) {
+
+router.post('/discover/yes', jsonParser, async(req, res) => {
     console.log("yes")
     res.redirect("/discover")
+    //make hardcoded matches
+    let hardCoded = true;
+
+    if(hardCoded){
+        const data = await Employee.findOne({_id : req.body.id});
+
+        let employer = {};
+        let employee = {};
+
+        if(data == undefined || data == null){ //employee clicking yes so randomize employee user
+            const employees = await Employee.find();
+            const randomEmployeeIndex = Math.floor(Math.random() * employees.length)
+
+            employee = employees[randomEmployeeIndex]
+            employer = await Employer.findOne({_id : req.body.id});
+            console.log(employer)
+        } else { //employer clicking yes so randomize employer user
+            const employers = await Employer.find();
+            const randomEmployerIndex = Math.floor(Math.random() * employers.length)
+
+            employer = employers[randomEmployerIndex]
+            employee = data
+        }
+
+        //make match object
+        //save it
+        //update employee/employeer
+
+        let newMatch = new Match({
+            employee : employee._id,
+            employer : employer._id
+        })
+
+        // if(employee.matches == undefined)
+        //     employee.matches = []
+        // else if(employer.matches == undefined)
+        //     employer.matches = []
+
+        try {
+            employee.matches.push(newMatch._id)
+            employer.matches.push(newMatch._id)
+
+            const dataToSave = await newMatch.save();
+            const updateEmployee = await Employee.findByIdAndUpdate(employee._id, employee, { new: true })
+            const updateEmployer = await Employee.findByIdAndUpdate(employer._id, employer, { new: true })
+
+            //res.status(200).json(dataToSave)
+
+            res.redirect("/discover")
+        }
+        catch (error) {
+            res.status(400).json({message: error.message})
+        }
+    }
+
 });
 
-router.get('/discover/no', function(req, res, next) {
+router.post('/discover/no', async(req, res) => {
     console.log("no")
     res.redirect("/discover")
 });
