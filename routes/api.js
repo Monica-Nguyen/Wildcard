500e3d1fce823c94ca1f27c7c71d5ad4f4e7d5ec
Anay <anaybhutoria@gmail.com>
2022-12-12 05:00:17 -0700

    creating login

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,10 @@ const Employee = require('../model/employee');
 const Employer = require('../model/employer')
 const Job = require('../model/job')
 const bodyParser = require("body-parser");
+const User = require("../model/user");
 const jsonParser = bodyParser.json();
+
+const mongoose = require('mongoose');
 
 module.exports = router;
 
@@ -198,3 +201,55 @@ router.post('/job/company',  jsonParser, async (req, res) => {
         res.status(400).json({message: error.message})
     }
 })
+
+//************************USER*****************************//
+
+// create User
+router.post('/user/create', jsonParser,  async (req, res) => {
+    const data = new User({
+        email: req.body.email,
+        password: req.body.password
+      
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+//check if a user with email and pass exists
+router.get('/user/exist', jsonParser,  async (req, res) => {
+    mongoose.model('user').findOne({"email": req.body.email}, function(error, exist) {
+        
+        console.log("req is :" ,req)
+        if(exist && !error){
+
+            res.status(200).send({"email_given": req.body.email, "bool" : 1, "message" : "DOES EXIST"})
+
+
+        }else if (!exist && !error){
+
+            res.status(200).send({"email_given" : req.body.email , "res" : 0 ,  "message" : "DOESNT EXIST"})
+        }else {
+        //IF YOU ARE USING EXPRESS.JS, YOU MUST USE RES.SEND() or RES.END() TO TERMINATE THE CONNECTION
+        res.status(500).send({"ERROR" : "Not Found"});
+        return;
+        }
+    })});
+
+
+
+    router.get('/user/all', async (req, res) => {
+        try{
+            const data = await User.find();
+            res.json(data)
+        }
+        catch(error){
+            res.status(500).json({message: error.message})
+        }
+    })
+    
