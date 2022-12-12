a1d783bbd1bed087f9f7226a2456565de4fa295d
Shailly Patel <shaillypatel@Shaillys-MacBook-Pro.local>
2022-12-12 00:34:27 -0700

    added employer profile pages

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/test', function(req, res, next) {
+   
+    res.render('test')
+
+});
 
 router.get('/employee', function(req, res, next) {
    
