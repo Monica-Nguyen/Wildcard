cb75428b15ff3498812607a0ed5d3fa327b28cd4
smitra33 <60757186+smitra33@users.noreply.github.com>
2022-12-14 17:05:30 -0700

    Fixed employee profile page route

--- routes/profile.js
+++ routes/profile.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const connectEnsureLogin = require("connect-ensure-login");
+const User = require("../model/user");
+const Employee = require("../model/employee");
+const Employer = require("../model/employer");
 const router = express.Router();
 
-router.get('/employee', function(req, res, next) {
+
+router.get('/employee/', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
    
     res.render('employee')
 
 });
 
-router.get('/employer', function(req, res, next) {
+router.get('/employer/', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
    
     res.render('employer')
 
