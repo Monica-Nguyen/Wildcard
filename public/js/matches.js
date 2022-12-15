204f18694030165b7ad4c69c87d22d06c1c9170c
smitra33 <60757186+smitra33@users.noreply.github.com>
2022-12-14 22:48:37 -0700

    Fixed matches page and chat

--- public/js/matches.js
+++ public/js/matches.js
@@ -7,21 +7,24 @@ async function getMatchByUser() {
             'Content-Type': 'application/json'
         },
     });
-    let data = await response.json();
+    let matchArray = await response.json();
     let matchList = ``;
-    console.log(data);
-    let matchArray = data.matches;
     if (matchArray.length > 0) {
-        matchArray.forEach(match =>{
-            let chatURL = "/chat/" + match;
+        for (let i=0; i < matchArray.length; i++) {
+            let response = await fetch (`http://localhost:3000/matches/getNames/`+ matchArray[i], {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+            let matchNames = await response.json();
+            let chatURL = "/chat/" + matchArray[i];
             matchList +=
                 `
-            <div>
-                <a class="match_link"href=${chatURL}><span class="dot">Oracle</span></a>
-            </div>
+                <a class="match_link" href=${chatURL}><span class="dot"><p style="margin-top: 80px">${matchNames.employee} and ${matchNames.employer}</p></span></a>
             
             `
-        });
+        }
     }
     else {
         matchList +=
