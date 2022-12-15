44d1739984c47efdf9e9b64b6aa4d7c59f9d47c1
smitra33 <60757186+smitra33@users.noreply.github.com>
2022-12-14 20:34:02 -0700

    Updated profile routing for nav bar

--- routes/profile.js
+++ routes/profile.js
@@ -6,6 +6,33 @@ const Employer = require("../model/employer");
 const router = express.Router();
 
 
+router.get('/', connectEnsureLogin.ensureLoggedIn(), async function (req, res, next) {
+    if (req.isAuthenticated()) {
+        let employee = await Employee.findOne({"user": req.user._id});
+        let employer = await Employer.findOne({"user": req.user._id});
+        if (employee != null) {
+            if (employee.length !== 0) {
+                res.redirect('/profile/employee')
+            }
+        } else if (employer != null) {
+            if (employer.length !== 0) {
+                res.redirect('/profile/employer')
+            }
+        } else {
+            res.redirect('account/')
+        }
+    } else {
+        res.render('login.ejs', {})
+    }
+});
+
+router.get('/employer/', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
+
+    res.render('employer')
+
+});
+
+
 router.get('/employee/', connectEnsureLogin.ensureLoggedIn(), function(req, res, next) {
    
     res.render('employee')
